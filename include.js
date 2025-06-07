15af9b457e204eb2da3fc0dc3a34f5ead2d584fc
Zakery Clarke <30702789+zakerytclarke@users.noreply.github.com>
2025-06-07 13:58:50 -0400

    Update include.js

--- include.js
+++ include.js
@@ -1,6 +1,7 @@
 // const bannerColor = "#b84041";
 document.getElementById("forkme_banner").style.display="none";
-
+document.getElementsByClassName("right-tail")[0].style.display = "none";
+document.getElementsByClassName("left-tail")[0].style.display = "none";
 // document.getElementsByClassName("ribbon-inner")[0].children[0].style.backgroundColor=bannerColor;
 // document.getElementsByClassName("ribbon-inner")[0].children[1].style.backgroundColor=bannerColor;
 
