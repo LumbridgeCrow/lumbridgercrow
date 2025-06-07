df6dc43640b268ae93e265617139d11b6660a5f8
Zakery Clarke <30702789+zakerytclarke@users.noreply.github.com>
2025-06-07 12:31:43 -0400

    Update include.js

--- include.js
+++ include.js
@@ -1,1 +1,5 @@
+const bannerColor = "#b84041";
 document.getElementById("forkme_banner").style.display="none";
+
+document.getElementsByClassName("ribbon-inner")[0].children[0].style.backgroundColor=bannerColor;
+document.getElementsByClassName("ribbon-inner")[0].children[1].style.backgroundColor=bannerColor;
